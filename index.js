1044bfb528304005fb140195d15ad6323bd7d872
DESKTOP-8CB3OHD\tombr <tom.brouder@hotmail.fr>
2017-07-18 23:49:56 +0200

    Temps d'attente à chaque arrêt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let express = require('express'); // on récupère la dépendance js
 
+let request = require('request');
+
 let app = express(); // On créé une application
 
 //app.use(express.static(__dirname + '/front'));  On dit queles fichiers statiques sont dans le dossier front. Pas utile pour ce projet
@@ -8,9 +10,19 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html')); // Connecti
 
 app.get('/about', (req, res) => res.sendFile(__dirname + '/about.html'));
 
-app.get('/hello', (req, res) => res.send("Bonjour "+ req.param('name')) ); // On récupère le query parameter 'name' i.e. ?id=name   Le ? dit différencie la route des query parameters
+app.get('/hello', (req, res) => res.send("Bonjour " + req.param('name'))); // On récupère le query parameter 'name' i.e. ?id=name   Le ? dit différencie la route des query parameters
 
-app.get('/book/:id', (req, res) => res.json({name:'book '+ req.params.id}));
+//app.get('/book/:id', (req, res) => res.json({name: 'book ' + req.params.id}));
+
+app.get('/tan/:id', (req, res) => {
+  request('http://open.tan.fr/ewp/tempsattente.json/'+req.params.id, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.json(JSON.parse(body))
+  });
+
+})
 
 app.listen(process.env.PORT || 3000, () => console.log('App running at http://localhost:3000')); // On lui dit sur quel port tourner
                                                                                                  // () => ... c'est callback= fonction qui est appelée une fois que c'est fini
